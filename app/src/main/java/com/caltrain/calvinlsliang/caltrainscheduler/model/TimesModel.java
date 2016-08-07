3c8f28c9e35579dd9768fdf6627c54521fa3a776
Calvin Liang <calvinlsliang@gmail.com>
2016-08-06 20:09:33 -0700

    Initial commit

---
+++ app/src/main/java/com/caltrain/calvinlsliang/caltrainscheduler/model/TimesModel.java
@@ -0,0 +1,8 @@
+package com.caltrain.calvinlsliang.caltrainscheduler.model;
+
+public class TimesModel {
+    public String startTime;
+    public String endTime;
+    public String busNumber;
+    public int duration;
+}
