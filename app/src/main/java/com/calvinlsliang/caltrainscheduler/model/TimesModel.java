51c0ac14deb732380eebf70efe9a17007b69667e
Calvin Liang <cliang@groupon.com>
2016-08-16 14:29:22 -0700

    Basic algo to figure out available times

--- app/src/main/java/com/calvinlsliang/caltrainscheduler/model/TimesModel.java
+++ app/src/main/java/com/calvinlsliang/caltrainscheduler/model/TimesModel.java
@@ -1,8 +1,36 @@
 package com.calvinlsliang.caltrainscheduler.model;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 public class TimesModel {
+
+    private final SimpleDateFormat format = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
     public String startTime;
     public String endTime;
     public String busNumber;
-    public int duration;
+    public String duration;
+
+    public TimesModel() {
+    }
+
+    public TimesModel(String startTime, String endTime, int busNumber) {
+        final DateFormat dateFormat = DateFormat.getTimeInstance(DateFormat.SHORT);
+        try {
+            final Date startDate = format.parse(startTime);
+            final Date endDate = format.parse(endTime);
+
+            this.startTime = dateFormat.format(startDate);
+            this.endTime = dateFormat.format(endDate);
+            this.duration = TimeUnit.MILLISECONDS.toMinutes(endDate.getTime() - startDate.getTime()) + " min";
+
+        } catch (ParseException | NullPointerException e) {
+        }
+        this.busNumber = "#" + busNumber;
+    }
 }
