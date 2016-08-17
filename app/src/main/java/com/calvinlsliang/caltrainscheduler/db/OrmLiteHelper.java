6bd730115c68deec987dcd1adbecbb8c1f9efbeb
Calvin Liang <cliang@groupon.com>
2016-08-16 21:46:47 -0700

    wip

--- app/src/main/java/com/calvinlsliang/caltrainscheduler/db/OrmLiteHelper.java
+++ app/src/main/java/com/calvinlsliang/caltrainscheduler/db/OrmLiteHelper.java
@@ -10,23 +10,29 @@ import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 
-public class CaltrainOrmLiteSqliteOpenHelper extends OrmLiteSqliteOpenHelper {
+public class OrmLiteHelper extends OrmLiteSqliteOpenHelper {
 
     private static final String DATABASE_NAME = "scheduler.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 1;
 
     private Dao<StopTimes, Long> stopTimes = null;
+    private Context context;
 
-    public CaltrainOrmLiteSqliteOpenHelper(Context context) {
+    public OrmLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION, R.raw.ormlite_config);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
         try {
             TableUtils.createTable(connectionSource, StopTimes.class);
+            populateStopTimes();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -34,13 +40,50 @@ public class CaltrainOrmLiteSqliteOpenHelper extends OrmLiteSqliteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
-
         try {
             TableUtils.dropTable(connectionSource, StopTimes.class, false);
             onCreate(database, connectionSource);
+            populateStopTimes();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+    }
+
+    // Populate db with static data from CSV
+    private void populateStopTimes() throws SQLException {
+
+        // trip_id,arrival_time,departure_time,stop_id,stop_sequence,pickup_type,drop_off_type
+
+        // TODO remove JSefa if this works
+
+        final String splitBy = ",";
+        String line;
+        String tripId;
+        String arrivalTime;
+        String stopId;
+
+        try {
+            final InputStreamReader is = new InputStreamReader(context.getAssets().open("stop_times.txt"));
+            final BufferedReader reader = new BufferedReader(is);
+
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(splitBy);
+                tripId = split[0];
+                arrivalTime = split[1];
+                stopId = split[3];
+
+            }
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
     public Dao<StopTimes, Long> getDao() throws SQLException {
@@ -48,5 +91,11 @@ public class CaltrainOrmLiteSqliteOpenHelper extends OrmLiteSqliteOpenHelper {
             stopTimes = getDao(StopTimes.class);
         }
         return stopTimes;
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        stopTimes = null;
     }
 }
--- app/src/main/java/com/calvinlsliang/caltrainscheduler/db/model/StopTimes.java
+++ app/src/main/java/com/calvinlsliang/caltrainscheduler/db/model/StopTimes.java
@@ -3,18 +3,25 @@ package com.calvinlsliang.caltrainscheduler.db.model;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+import org.jsefa.csv.annotation.CsvDataType;
+import org.jsefa.csv.annotation.CsvField;
+
 @DatabaseTable(tableName = "StopTimes")
+@CsvDataType()
 public class StopTimes {
 
     @DatabaseField(generatedId = true)
     private long id;
 
     @DatabaseField(columnName = "trip_id")
+    @CsvField(pos = 1)
     private String tripId;
 
     @DatabaseField(columnName = "stop_id")
+    @CsvField(pos = 4)
     private String stopId;
 
     @DatabaseField(columnName = "arrival_time")
+    @CsvField(pos = 2)
     private String arrivalTime;
 }
