a8df99d97f12a684018b576a4c60b5bd69380840
Calvin Liang <calvinlsliang@gmail.com>
2016-08-23 23:46:39 -0700

    Decrease db version back to 1

--- app/src/main/java/com/calvinlsliang/caltrainscheduler/db/DatabaseHelper.java
+++ app/src/main/java/com/calvinlsliang/caltrainscheduler/db/DatabaseHelper.java
@@ -19,7 +19,7 @@ import java.sql.SQLException;
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
     private static final String DATABASE_NAME = "scheduler.db";
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 1;
 
     private Dao<StopTimes, Long> stopTimes = null;
     private Context context;
